13880cde879b75e99a645c1b8e06c898a40485c4
Jocelyn Jayjack <jjayjack@dminc.com>
2022-07-01 13:31:44 -0500

    Spell Check: it's a thing

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
 export GITHUB_USERNAME="jjayjack"
-export GITHUB_REPO="bootcamp-2022-sls-api-jayjackGit"
+export GITHUB_REPO="bootcamp-2022-sls-api-jjayjackGit"
 export BUCKET_NAME="jjayjack-bootcamp-2022-bucket" #username must be all lowercase
