9ab2bf646fd8165837b80c79da3989dfc7e0c570
Jocelyn Jayjack <jjayjack@dminc.com>
2022-07-01 12:23:31 -0500

    Starting files for Serverless API

---
+++ setEnvVars.sh
@@ -0,0 +1,3 @@
+export GITHUB_USERNAME="<<username>>"
+export GITHUB_REPO="bootcamp-2022-sls-api-<<lastname>>.git"
+export BUCKET_NAME="<<username>>-bootcamp-2022-bucket" #username must be all lowercase
