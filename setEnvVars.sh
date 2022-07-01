13d891bd569afdb191a4ff51c974c6767c88c5b5
Jocelyn Jayjack <jjayjack@dminc.com>
2022-07-01 12:32:00 -0500

    updated GIhub Env

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
-export GITHUB_USERNAME="<<username>>"
-export GITHUB_REPO="bootcamp-2022-sls-api-<<lastname>>.git"
-export BUCKET_NAME="<<username>>-bootcamp-2022-bucket" #username must be all lowercase
+export GITHUB_USERNAME="jjayjack"
+export GITHUB_REPO="bootcamp-2022-sls-api-jayjackGit"
+export BUCKET_NAME="jjayjack-bootcamp-2022-bucket" #username must be all lowercase
